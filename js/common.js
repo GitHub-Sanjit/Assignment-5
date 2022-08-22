b2ce8b1b26d5d8d555a1a5508c34b53014b72d1a
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-08-22 23:58:53 +0600

    Selected player limit functionality added

--- js/common.js
+++ js/common.js
@@ -3,7 +3,19 @@ function appendListItem(cardTitleName) {
     const titleNameElement = document.getElementById(cardTitleName);
     const titleName = titleNameElement.innerText;
     const playerList = document.getElementById('player-list');
-    const li = document.createElement('li');
+    
+    // console.log(playerList.childNodes);
+    // console.log(playerList.childNodes.length);
+
+    if (playerList.childNodes.length <= 5) {
+        const li = document.createElement('li');
     li.innerText = titleName;
+    // console.log(li);
     const liName = playerList.appendChild(li); 
+    }
+    else {
+        alert("You Have Reached Your Limit");
+    }
+    
+    
 }
