d2b1ab25196bd5803b59bb7d31fe89667a07af42
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-08-22 15:07:51 +0600

    Button disabled Functionality

--- js/app.js
+++ js/app.js
@@ -2,6 +2,42 @@ const messiBtn = document.getElementById('select-messi');
 messiBtn.addEventListener('click', function () {
     // alert('Messi Clicked');
     appendList('title-messi');
+    messiBtn.setAttribute('disabled', 'disabled');
+})
+
+const neymarBtn = document.getElementById('select-neymar');
+neymarBtn.addEventListener('click', function () {
+    // alert('Messi Clicked');
+    appendList('title-neymar');
+    neymarBtn.setAttribute('disabled', 'disabled');
+})
+
+const kylianBtn = document.getElementById('select-kylian');
+kylianBtn.addEventListener('click', function () {
+    // alert('Messi Clicked');
+    appendList('title-kylian');
+    kylianBtn.setAttribute('disabled', 'disabled');
+})
+
+const renatoBtn = document.getElementById('select-renato');
+renatoBtn.addEventListener('click', function () {
+    // alert('Messi Clicked');
+    appendList('title-renato');
+    renatoBtn.setAttribute('disabled', 'disabled');
+})
+
+const sergioBtn = document.getElementById('select-sergio');
+sergioBtn.addEventListener('click', function () {
+    // alert('Messi Clicked');
+    appendList('title-sergio');
+    sergioBtn.setAttribute('disabled', 'disabled');
+})
+
+const vitorBtn = document.getElementById('select-vitor');
+vitorBtn.addEventListener('click', function () {
+    // alert('Messi Clicked');
+    appendList('title-vitor');
+    vitorBtn.setAttribute('disabled', 'disabled');
 })
 
 
