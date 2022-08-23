810dc8b8c24ce34941fd3bf54348a267492c71b7
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-08-23 17:10:46 +0600

    All functionality done

---
+++ js/calculate.js
@@ -0,0 +1,21 @@
+
+document.getElementById('calculate').addEventListener('click', function () {
+    const selectedFive = document.getElementById('player-list');
+    const playerExpenses = document.getElementById('playerExpenses');
+    const perPlayerInput = document.getElementById('perPlayer');
+    let perPlayerInputValue = parseInt(perPlayerInput.value);
+    const playerCosts = (selectedFive.childNodes.length - 1) * perPlayerInputValue;
+    playerExpenses.innerText = playerCosts;
+})
+
+
+document.getElementById('calculateTotal').addEventListener('click', function () {
+    const playerExpenses = parseInt(document.getElementById('playerExpenses').innerText);
+    const managerCost = parseInt(document.getElementById('manager').value);
+    const coachCost = parseInt(document.getElementById('coach').value);
+    const totalCost = document.getElementById('total');
+
+    let total = playerExpenses - (managerCost + coachCost);
+
+    totalCost.innerText = total;
+})
--- js/common.js
+++ js/common.js
@@ -4,18 +4,13 @@ function appendListItem(cardTitleName) {
     const titleName = titleNameElement.innerText;
     const playerList = document.getElementById('player-list');
     
-    // console.log(playerList.childNodes);
-    // console.log(playerList.childNodes.length);
-
-    if (playerList.childNodes.length <= 5) {
-        const li = document.createElement('li');
-    li.innerText = titleName;
-    // console.log(li);
-    const liName = playerList.appendChild(li); 
-    }
-    else {
+    if (playerList.childNodes.length > 5) {
         alert("You Have Reached Your Limit");
     }
-    
-    
+    else {
+        const li = document.createElement('li');
+        li.innerText = titleName;
+        const liName = playerList.appendChild(li);
+    }
 }
+
